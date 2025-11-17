6667e9081a7053fc58d2e57eae09a2d4a25cfe35
Samuel Lampa <samuel.lampa@scilifelab.se>
2025-11-17 16:26:31 +0100

    Add a sleep before moving focus, so as not to cut scanned codes in two

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -176,6 +177,7 @@ func (e *ForwardJumpOnReturnEntry) SetNext(entry *ForwardJumpOnReturnEntry) {
 func (e *ForwardJumpOnReturnEntry) TypedKey(key *fyne.KeyEvent) {
 	if key.Name == fyne.KeyReturn {
 		if e.next != nil {
+			time.Sleep(250 * time.Millisecond)
 			e.next.SetPlaceHolder("Enter Sample ID now!")
 			e.canvas.Focus(e.next)
 			return
